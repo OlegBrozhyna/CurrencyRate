90b736a4bbf0695bf862e8c225fb04a35d3daf16
OlegBrozhyna <122048830+OlegBrozhyna@users.noreply.github.com>
2023-03-04 11:30:38 +0200

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.io.IOException;
+
+public class Main {
+
+    static String JSON_TEXT;
+
+    static {
+        JSON_TEXT = """
+                    [
+
+                    {
+                        "ccy":"EUR",
+                            "base_ccy":"UAH",
+                            "buy":"40.00000",
+                            "sale":"41.00000"
+                    },
+
+                    {
+                        "ccy":"USD",
+                            "base_ccy":"UAH",
+                            "buy":"38.40000",
+                            "sale":"38.90000"
+                    }]
+
+                    public static void main(String[] args) {
+
+                    }
+                }
+
+         
+         
+                """;
+    }
+
+    public static void main(String[] args) throws IOException {
+        GsonParser.parseJson("https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5");
+
+    }
+}
+
+
